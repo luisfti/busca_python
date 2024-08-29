5bfaa2b90542a01bd6f550b8f0ed90845a809cbc
Luis <luis100o_o@hotmail.com>
2024-08-29 12:52:15 -0300

    update insertion

--- T2/Insertion.py
+++ T2/Insertion.py
@@ -1,8 +1,21 @@
+# Insertion Sort
 def insertion_sort(arr):
-    for i in range(1, len(arr)):
+    n = len(arr)
+    max_val = arr[0]
+    for i in range(1, n):
         key = arr[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
+
+        # Insere o elemento na posição correta
+        while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
             j -= 1
+        
         arr[j + 1] = key
+
+        # Atualiza o máximo atual
+        if key > max_val:
+            max_val = key
+    
+    
+    return max_val
