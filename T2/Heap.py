694fbfaf114d5211e77f52675c147e30987814d9
Luis <luis100o_o@hotmail.com>
2024-09-02 22:39:26 -0300

    update complex

--- T2/Heap.py
+++ T2/Heap.py
@@ -1,32 +1,24 @@
-# Ordenação de raiz usando Heap
 def sqrt_heap_sort(arr):
     n = len(arr)
-    block_size = int(math.sqrt(n))  # Tamanho do bloco
-    
-    # Dividindo a lista em blocos
+    block_size = int(math.sqrt(n))
     blocks = [arr[i:i+block_size] for i in range(0, n, block_size)]
     
-    # Construindo heaps máximos de cada bloco
     block_heaps = []
     for block in blocks:
-        heapq._heapify_max(block)  # Construtor para heaps máximos
+        heapq._heapify_max(block)
         block_heaps.append(block)
     
-    # Construção da heap auxiliar
     max_heap = []
     for i in range(len(block_heaps)):
         if block_heaps[i]:
-            heapq.heappush(max_heap, (-block_heaps[i][0], i))  # Usa - para simular heap máximo
+            heapq.heappush(max_heap, (-block_heaps[i][0], i))
     
-    # Ordenando os elementos
     sorted_arr = []
     while max_heap:
-        max_value, block_index = heapq.heappop(max_heap)  # Remove o maior elemento
-        sorted_arr.append(-max_value)  # Adiciona o valor original ao resultado
+        max_value, block_index = heapq.heappop(max_heap)
+        sorted_arr.append(-max_value)
         if block_heaps[block_index]:
-            # Remove o maior elemento do bloco
             heapq._heappop_max(block_heaps[block_index])
-            # Se ainda há elementos no bloco, re-adiciona o maior elemento
             if block_heaps[block_index]:
                 heapq.heappush(max_heap, (-block_heaps[block_index][0], block_index))
     
--- T2/Insertion.py
+++ T2/Insertion.py
@@ -1,9 +1,25 @@
-# Insertion Sort
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]  # Elemento a ser comparado
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key  # Insere o elemento na posição correta
+def sqrt_heap_sort(arr):
+    n = len(arr)
+    block_size = int(math.sqrt(n))
+    blocks = [arr[i:i+block_size] for i in range(0, n, block_size)]
+    block_heaps = []
+
+    for block in blocks:
+        heapq._heapify_max(block)
+        block_heaps.append(block)
+    
+    max_heap = []
+    for i in range(len(block_heaps)):
+        if block_heaps[i]:
+            heapq.heappush(max_heap, (-block_heaps[i][0], i))  
+    
+    sorted_arr = []
+    while max_heap:
+        max_value, block_index = heapq.heappop(max_heap)
+        sorted_arr.append(-max_value)
+        if block_heaps[block_index]:
+            heapq._heappop_max(block_heaps[block_index])
+            if block_heaps[block_index]:
+                heapq.heappush(max_heap, (-block_heaps[block_index][0], block_index))
+    
+    return sorted_arr
